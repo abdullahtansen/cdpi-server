31f11c776e4b49529fe79186269afb5d31043463
Abdullah Al Tansen <abdullahaltansen899@gmail.com>
2022-04-23 18:04:17 +0600

    updtaed all

--- index.js
+++ index.js
@@ -22,49 +22,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     client.connect();
-    const database = client.db(`${process.env.DB_NAME}`);
-    const mobileCollection = database.collection("all_mobile");
-    const reviewsCollection = database.collection("reviews");
-    const orderCollection = database.collection("orders");
+    const database = client.db("cdpi");
+    const noticeCollection = database.collection("notice");
     const usersCollection = database.collection("users");
 
-    // GET all products data
+    // GET all notice data
 
-    app.get("/all-products", async (req, res) => {
-      const cursor = mobileCollection.find({});
-      const products = await cursor.toArray();
-      res.json(products);
+    app.get("/all-notice", async (req, res) => {
+      const cursor = noticeCollection.find({});
+      const notice = await cursor.toArray();
+      res.json(notice);
     });
 
-    // GET a single place by ID
+    // GET a single notice by ID
 
-    app.get("/product/:id", async (req, res) => {
+    app.get("/notice/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const mobile = await mobileCollection.findOne(query);
-      res.send(mobile);
-    });
-
-    // GET all reviews data
-
-    app.get("/all-reviews", async (req, res) => {
-      const cursor = reviewsCollection.find({});
-      const allReviews = await cursor.toArray();
-      res.json(allReviews);
-    });
-
-    // GET all orders data
-
-    app.get("/all-orders", async (req, res) => {
-      let query = {};
-      const email = req.query.email;
-      console.log(email);
-      if (email) {
-        query = { email };
-      }
-      const cursor = orderCollection.find(query);
-      const allOrders = await cursor.toArray();
-      res.json(allOrders);
+      const notice = await noticeCollection.findOne(query);
+      res.send(notice);
     });
 
     // GET admin or not
@@ -80,27 +56,11 @@ async function run() {
       res.json({ admin: isAdmin });
     });
 
-    // POST a single product
+    // POST a single notice
 
-    app.post("/add-product", async (req, res) => {
-      const mobile = req.body;
-      const result = await mobileCollection.insertOne(mobile);
-      res.json(result);
-    });
-
-    // POST a order
-
-    app.post("/order", async (req, res) => {
-      const order = req.body;
-      const result = await orderCollection.insertOne(order);
-      res.json(result);
-    });
-
-    // POST a single review
-
-    app.post("/add-reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewsCollection.insertOne(review);
+    app.post("/add-notice", async (req, res) => {
+      const notice = req.body;
+      const result = await noticeCollection.insertOne(notice);
       res.json(result);
     });
 
@@ -127,47 +87,12 @@ async function run() {
       res.json(result);
     });
 
-    // PUT admin & check with JWT Token  he/she is admin or not ?
-
-    app.put("/users/admin", async (req, res) => {
-      const newAdmin = req.body;
-
-      const filter = { email: newAdmin.email };
-      const updateUser = { $set: { role: "Admin" } };
-      const result = await usersCollection.updateOne(filter, updateUser);
-      res.json(result);
-    });
-
-    // PUT order status
-
-    app.put("/order/:id", async (req, res) => {
-      const id = req.params.id;
-      const status = req.body.status;
-      const filter = { _id: ObjectId(id) };
-      const updatingStatus = {
-        $set: {
-          status: status,
-        },
-      };
-      const result = await orderCollection.updateOne(filter, updatingStatus);
-      res.json(result);
-    });
-
     // DELETE a single product by ID
 
-    app.delete("/product/:id", async (req, res) => {
+    app.delete("/notice/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await mobileCollection.deleteOne(query);
-      res.json(result);
-    });
-
-    // DELETE a single order by ID
-
-    app.delete("/order/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await orderCollection.deleteOne(query);
+      const result = await noticeCollection.deleteOne(query);
       res.json(result);
     });
   } finally {
@@ -177,5 +102,5 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get("/", (req, res) => res.send("Welcome to Mobile Store Server API"));
+app.get("/", (req, res) => res.send("Welcome to CDPI Server API"));
 app.listen(port, () => console.log(`Server Running on localhost:${port}`));
